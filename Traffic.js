3d3d378904303620671c8a8b6b84822387a4509f
Billy008-001 <bgathecere2@gmail.com>
2026-03-11 08:16:55 +0300

    Introduced the image showcase part

--- Traffic.js
+++ Traffic.js
@@ -15,6 +15,23 @@ function pedestrianLight(event) {
         alertMessage.style.color = "red";
         alertMessage.style.fontFamily = "Gill Sans";
         alertMessage.style.fontSize = "1.5rem";
+
+        // Introduces an image of the traffic lights to show red lights
+
+        // Fetch the div to put the image
+        const pedestrianImage = document.getElementById("pedestrian-lights");   
+
+        // Create the image
+        const redImage = document.createElement("img");
+        redImage.src = "./Images/Pedestrians stop.webp";
+        redImage.alt = "Pedestrians get ready";
+
+        // Set the width and height of the image
+        redImage.width = 300;
+        redImage.height = 300;
+
+        // Append the image to the div
+        pedestrianImage.appendChild(redImage);
         return true;
 
     }
@@ -26,7 +43,26 @@ function pedestrianLight(event) {
         alertMessage.style.color = "yellow";
         alertMessage.style.fontFamily = "Gill Sans";
         alertMessage.style.fontSize = "1.5rem";
-        return true;
+        
+        // Introduces an image of the traffic lights to show yellow lights
+
+        // Fetch the div to put the image
+        const pedestrianImage = document.getElementById("pedestrian-lights");
+
+        // Create the image
+        const yellowImage = document.createElement("img");
+        yellowImage.src = "./Images/Pedestrians get ready.webp";
+        yellowImage.alt = "Pedestrians get ready";
+
+        // Set the width and height of the image
+        yellowImage.width = 300;
+        yellowImage.height = 300;
+
+        // Append the image to the div
+
+        pedestrianImage.innerHTML = ""; // removes any existing image
+        pedestrianImage.appendChild(yellowImage);
+
 
     }
     else if(pedestrianLights.classList.contains("light-green")){
@@ -38,10 +74,23 @@ function pedestrianLight(event) {
         alertMessage.style.color = "green";
         alertMessage.style.fontFamily = "Gill Sans";
         alertMessage.style.fontSize = "1.5rem";
-        return true;
+
+
 
     }
-    else{
-        return false;
+    else if(pedestrianLights.classList.contains("light-green")){
+
+        alertMessage.textContent = "Go! The traffic light is green! You can go down the road.";
+
+
+        //Change the font color to green, font family to Gill Sans and font size to 1.5rem     
+        alertMessage.style.color = "green";
+        alertMessage.style.fontFamily = "Gill Sans";
+        alertMessage.style.fontSize = "1.5rem";
+
+
     }
+
+    return true;
+
 }
