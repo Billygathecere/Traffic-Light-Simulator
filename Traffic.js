02f562b20a96f6f4b6048323beacf5121143c2c8
Billy008-001 <bgathecere2@gmail.com>
2026-03-11 09:21:58 +0300

    Added the alert message for the vehicle side and styled it too

--- Traffic.js
+++ Traffic.js
@@ -16,22 +16,7 @@ function pedestrianLight(event) {
         alertMessage.style.fontSize = "1.5rem";
 
         // Introduces an image of the traffic lights to show red lights
-
-        // Fetch the div to put the image
-        const pedestrianImage = document.getElementById("pedestrian-lights");   
-
-        // Create the image
-        const redImage = document.createElement("img");
-        redImage.src = "./Images/Pedestrians stop.webp";
-        redImage.alt = "Pedestrians get ready";
-
-        // Set the width and height of the image
-        redImage.width = 300;
-        redImage.height = 300;
-
-        // Append the image to the div
-        pedestrianImage.appendChild(redImage);
-        return true;
+        updatePedestrianLights("./Images/Pedestrians stop.webp");
 
     }
     else if(pedestrianLights.classList.contains("light-yellow")){
@@ -43,23 +28,7 @@ function pedestrianLight(event) {
         alertMessage.style.fontSize = "1.5rem";
         
         // Introduces an image of the traffic lights to show yellow lights
-
-        // Fetch the div to put the image
-        const pedestrianImage = document.getElementById("pedestrian-lights");
-
-        // Create the image
-        const yellowImage = document.createElement("img");
-        yellowImage.src = "./Images/Pedestrians get ready.webp";
-        yellowImage.alt = "Pedestrians get ready";
-
-        // Set the width and height of the image
-        yellowImage.width = 300;
-        yellowImage.height = 300;
-
-        // Append the image to the div
-
-        pedestrianImage.innerHTML = ""; // removes any existing image
-        pedestrianImage.appendChild(yellowImage);
+        updatePedestrianLights("./Images/Pedestrians get ready.webp");
 
 
     }
@@ -72,22 +41,81 @@ function pedestrianLight(event) {
         alertMessage.style.color = "green";
         alertMessage.style.fontSize = "1.5rem";
 
-
-
-    }
-    else if(pedestrianLights.classList.contains("light-green")){
-
-        alertMessage.textContent = "Go! The traffic light is green! You can go down the road.";
-
-
-        //Change the font color to green, font family to Gill Sans and font size to 1.5rem     
-        alertMessage.style.color = "green";
-        alertMessage.style.fontFamily = "Gill Sans";
-        alertMessage.style.fontSize = "1.5rem";
-
+        // Introduces an image of the traffic lights to show green lights
+        updatePedestrianLights("./Images/Pedestrians go.webp");
 
     }
-
     return true;
 
+}
+
+const updatePedestrianLights = (imagePath) => {
+    const pedestrianLights = document.getElementById("pedestrian-lights");
+    pedestrianLights.innerHTML = "";
+    const image = document.createElement("img");
+    image.src = imagePath;
+    image.width = 300;
+    image.height = 300;
+    pedestrianLights.appendChild(image);
+}
+
+// Vehicles side 
+
+function carLight(event) {
+    // the event.target is the element that was clicked, it's the light that was clicked
+    const vehicleLights = event.target;
+    const alertMessage = document.getElementById("alertNotification");
+    
+    alertMessage.textContent = "";
+
+    if(vehicleLights.classList.contains("light-red")){
+
+        alertMessage.textContent = "Stop! The traffic light is red! If you want to go down the road, you have to wait for the green light.";
+
+        //Change the font color to red, font family to Gill Sans and font size to 1.5rem     
+        alertMessage.style.color = "red";
+        alertMessage.style.fontSize = "1.5rem";
+
+        // Introduces an image of the traffic lights to show red lights
+        updateVehicleLights("./Images/Vehicles stop.webp");
+
+    }
+    else if(vehicleLights.classList.contains("light-yellow")){
+
+        alertMessage.textContent = "Slow down! The traffic light is yellow! If you want to go down the road, you have to wait for the green light.";
+
+        //Change the font color to yellow, font family to Gill Sans and font size to 1.5rem     
+        alertMessage.style.color = "yellow";
+        alertMessage.style.fontSize = "1.5rem";
+        
+        // Introduces an image of the traffic lights to show yellow lights
+        updateVehicleLights("./Images/Vehicles get ready.webp");
+
+
+    }
+    else if(vehicleLights.classList.contains("light-green")){
+
+        alertMessage.textContent = "Go! The traffic light is green! You can go down the road.";
+
+
+        //Change the font color to green, font family to Gill Sans and font size to 1.5rem     
+        alertMessage.style.color = "green";
+        alertMessage.style.fontSize = "1.5rem";
+
+        // Introduces an image of the traffic lights to show green lights
+        updateVehicleLights("./Images/Vehicles go.webp");
+
+    }
+    return true;
+
+}
+
+const updateVehicleLights = (imagePath) => {
+    const vehicleLights = document.getElementById("car-lights");
+    vehicleLights.innerHTML = "";
+    const image = document.createElement("img");
+    image.src = imagePath;
+    image.width = 300;
+    image.height = 300;
+    vehicleLights.appendChild(image);
 }
